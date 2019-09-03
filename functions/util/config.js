0657aee0dd9b9c21023cfff9204712209c5511b9
chiragw34 <43503200+chiragw34@users.noreply.github.com>
2019-09-03 15:50:36 +0530

    deleted config.js

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports= {
-    apiKey: "AIzaSyBvMmRfrXtFUQ7h6pD49hG7J9jt5RPW6sY",
-    authDomain: "socialapp-40c09.firebaseapp.com",
-    databaseURL: "https://socialapp-40c09.firebaseio.com",
-    projectId: "socialapp-40c09",
-    storageBucket: "socialapp-40c09.appspot.com",
-    messagingSenderId: "756357702067",
-    appId: "1:756357702067:web:17c944fe323b129c"
-  };
