4fb6fe092ed9526c5e77c28fa2ef4c207adf471b
Chirag Wadhwa <chiragw34@gmail.com>
2019-09-04 15:11:50 +0530

    add and get user profile details

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -5,8 +5,13 @@ const firebaseConfig = require("../util/config");
 const firebase = require("firebase");
 firebase.initializeApp(firebaseConfig);
 
-const { validateSignupData, validateLoginData } = require("../util/validators");
+const {
+  validateSignupData,
+  validateLoginData,
+  reduceUserDetails
+} = require("../util/validators");
 
+// sign up users
 exports.signup = (req, res) => {
   const newUser = {
     email: req.body.email,
@@ -61,6 +66,7 @@ exports.signup = (req, res) => {
     });
 };
 
+//logging in users
 exports.login = (req, res) => {
   const user = {
     email: req.body.email,
@@ -90,6 +96,48 @@ exports.login = (req, res) => {
     });
 };
 
+//add user details
+exports.addUserDetails = (req, res) => {
+  let userDetails = reduceUserDetails(req.body);
+  db.doc(`/users/${req.user.handle}`)
+    .update(userDetails)
+    .then(() => {
+      return res.json({ message: "details added successfully" });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+//Get own user details
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db
+          .collection("likes")
+          .where("userHandle", "==", req.user.handle)
+          .get();
+      }
+    })
+    .then(data => {
+      userData.likes = [];
+      data.forEach(doc => {
+        userData.likes.push(doc.data());
+      });
+      return res.json(userData);
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+//upload profile image
 exports.uploadImage = (req, res) => {
   const BusBoy = require("busboy");
   const path = require("path");
--- functions/index.js
+++ functions/index.js
@@ -5,19 +5,25 @@ const app = require("express")();
 const FBAuth = require("./util/fbAuth");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage } = require("./handlers/users");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require("./handlers/users");
 
 //Scream routes
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 //signup route
 app.post("/signup", signup);
 
 //login route
 app.post("/login", login);
-
-//Image upload route
-app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.region("asia-east2").https.onRequest(app);
